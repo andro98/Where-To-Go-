4d68765f7dc302aa737462453b6c46a444ea3f29
unknown <mariageorge291@gmail.com>
2019-02-22 14:23:53 +0200

    NEWS

---
+++ app/src/main/java/com/example/wheretogo/NewsMap.java
@@ -0,0 +1,13 @@
+package com.example.wheretogo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_map2);
+    }
+}
